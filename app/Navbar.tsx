f0c10290edf635926172edb116e0f0fb6a333f05
Kushalchavan <kushalchavan2002@gmail.com>
2024-09-04 23:10:41 +0530

    implement prisma

---
+++ app/Navbar.tsx
@@ -0,0 +1,40 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+import { AiFillBug } from "react-icons/ai";
+import classnames from "classnames";
+
+const Navbar = () => {
+  const currentPath = usePathname();
+
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
+
+  return (
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href="/">
+        <AiFillBug />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map((item) => (
+          <Link
+            key={item.href}
+            className={classnames({
+                'text-zinc-900' : item.href === currentPath,
+                'text-zinc-500' : item.href !== currentPath,
+                'hover:text-zinc-800 transition-colors': true
+            })}
+            href={item.href}
+          >
+            {item.label}
+          </Link>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,13 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import "./theme-config.css";
+import Navbar from "./Navbar";
+import "@radix-ui/themes/styles.css";
+import { Theme, ThemePanel } from "@radix-ui/themes";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +21,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.variable}>
+        <Theme accentColor="plum">
+          <Navbar />
+          <main className="p-5">{children}</main>
+        </Theme>
+      </body>
     </html>
   );
 }
